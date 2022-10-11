06933f103350ed473263099fe87ecdb9c9e290c5
jwlh00 <juunshuu00@gmail.com>
2022-10-11 15:45:40 -0600

    final

--- Raytracer.py
+++ Raytracer.py
@@ -9,16 +9,9 @@ height = 512
 
 # Materiales
 
-brick = Material(diffuse = (0.8, 0.3, 0.3), spec = 16)
-stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
-grass = Material(diffuse = (0.3, 1.0, 0.3), spec = 64)
-
-
-marble = Material(spec = 64, texture = Texture("marble.bmp"), matType= REFLECTIVE)
-
+marble = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = OPAQUE)
 mirror = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
 glass = Material(diffuse = (0.9, 0.9, 0.9), spec = 64, ior = 1.5, matType = TRANSPARENT)
-
 rtx = Raytracer(width, height)
 
 rtx.envMap = Texture("parkingLot.bmp")
@@ -28,16 +21,9 @@ rtx.lights.append( AmbientLight(intensity = 0.1 ))
 rtx.lights.append( PointLight(point = (-1,-1,0) ))
 
 
-rtx.scene.append( Plane(position = (0,-10,0), normal = (0,1,0), material = brick ))
-rtx.scene.append( Plane(position = (0,10,0), normal = (0,-1,0), material = brick ))
-rtx.scene.append( Plane(position = (-10,0,0), normal = (1,0,0), material = stone ))
-rtx.scene.append( Plane(position = (10,0,0), normal = (-1,0,0), material = stone ))
-rtx.scene.append( Plane(position = (0,0,-40), normal = (0,0,1), material = stone ))
-
-rtx.scene.append( Disk(position = (0,-3,-7), radius = 2, normal = (0,1,0), material = mirror ))
-
-rtx.scene.append( AABB(position = (-2,1,-10), size = (2,2,2), material = glass))
-rtx.scene.append( AABB(position = (2,1,-10), size = (2,2,2), material = marble))
+rtx.scene.append( Disk(position = (0,-2,-7), radius = 2, normal = (0,1,0), material = mirror ))
+rtx.scene.append( Disk(position = (-2.2,1,-7), radius = 1, normal = (0,0,1), material = glass ))
+rtx.scene.append( Disk(position = (1.8,1,-7), radius = 1.5, normal = (0,0,1), material = marble ))
 
 rtx.glRender()
 
